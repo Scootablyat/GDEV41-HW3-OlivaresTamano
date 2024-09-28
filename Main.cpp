c23568796f9f9b9da003d45c0b4fdb5f1b6b3bc0
csci40 finalproject <christianolivares13@gmail.com>
2024-09-28 19:24:26 +0800

    added drag controls. no limit implemented

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,5 @@
+//  g++ Main.cpp -o Main -I raylib/ -L raylib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+//  ./Main.exe
 #include <raylib.h>
 #include <raymath.h>
 #include <iostream>
@@ -7,10 +9,10 @@ const int WINDOW_HEIGHT = 600;
 const int numberOfCircles = 3;
 const float FPS = 60;
 const float TIMESTEP = 1 / FPS; // Sets the timestep to 1 / FPS. But timestep can be any very small value.
-const float FRICTION = 0.5;
+const float FRICTION = 2.5;
 
-
-struct Ball {
+struct Ball
+{
     Vector2 position;
     float radius;
     Color color;
@@ -21,29 +23,52 @@ struct Ball {
     Vector2 velocity;
 };
 
-int toggleElasticity(int e){
-    if(e == 0){
+int toggleElasticity(int e)
+{
+    if (e == 0)
+    {
         return 1;
     }
     return 0;
 }
 
-float getDistance(Ball b1, Ball b2){
+float getDistance(Ball b1, Ball b2)
+{
     Vector2 dist = Vector2Subtract(b1.position, b2.position);
     return std::abs(Vector2Length(dist));
 }
 
-bool isCirclesColliding(Ball b1, Ball b2){
-    float sumOfRadii = b1.radius + b2.radius; 
+bool isCirclesColliding(Ball b1, Ball b2)
+{
+    float sumOfRadii = b1.radius + b2.radius;
     float distance = getDistance(b1, b2);
-    if(distance <= sumOfRadii){
+    if (distance <= sumOfRadii)
+    {
         std::cout << "colliding" << std::endl;
         return true;
     }
     return false;
 }
 
-int main() {
+// void DragCueBall(Vector2 *vector)
+// {
+// }
+
+// float draglineX = abs(dragLine0->x - dragLine1->x);
+// float draglineY = abs(dragLine0->y - dragLine1->y);
+// if (draglineX > 200.0f)
+// {
+//     std::cout << Vector2Distance(*dragLine0, *dragLine1) << std::endl;
+//     dragLine1->x = dragLine0->x + 200.0f;
+// }
+// if (draglineY > 200.0f)
+// {
+//     // std::cout << Vector2Distance(*dragLine0, *dragLine1) << std::endl;
+//     dragLine1->y = dragLine0->y + 200.0f;
+// }
+
+int main()
+{
     Ball ball;
     ball.position = {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2};
     ball.radius = 30.0f;
@@ -63,7 +88,7 @@ int main() {
     ball2.velocity = Vector2Zero();
 
     Ball ball3;
-    ball3.position = {WINDOW_WIDTH- 200, WINDOW_HEIGHT- 200};
+    ball3.position = {WINDOW_WIDTH - 200, WINDOW_HEIGHT - 200};
     ball3.radius = 30.0f;
     ball3.color = YELLOW;
     ball3.mass = 1.0f;
@@ -75,68 +100,108 @@ int main() {
 
     int elasticityCoefficient = 0;
 
+    // Vector2 *dragLine0 = new Vector2;
+    // Vector2 *dragLine1 = new Vector2;
+    // Vector2 *dragLine1 = new Vector2{0.0f, 0.0f};
+
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tamano - Exercise 4");
 
     SetTargetFPS(FPS);
 
     float accumulator = 0;
 
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose())
+    {
         float delta_time = GetFrameTime();
         Vector2 forces = Vector2Zero();
-        if(IsKeyPressed(KEY_SPACE)){
+        Vector2 *dragLine0 = new Vector2;
+        Vector2 *dragLine1 = new Vector2;
+        if (IsKeyPressed(KEY_SPACE))
+        {
             elasticityCoefficient = toggleElasticity(elasticityCoefficient);
             std::cout << "elasticity: " << elasticityCoefficient << std::endl;
         }
 
-        if(IsKeyDown(KEY_W)) {
-            //std::cout << ball.acceleration.x << " " << ball.acceleration.y << std::endl;
+        if (IsKeyDown(KEY_W))
+        {
+            // std::cout << ball.acceleration.x << " " << ball.acceleration.y << std::endl;
             forces = Vector2Add(forces, {0, -100});
         }
-        if(IsKeyDown(KEY_A)) {
-            //std::cout << ball.acceleration.x << " " << ball.acceleration.y << std::endl;
+        if (IsKeyDown(KEY_A))
+        {
+            // std::cout << ball.acceleration.x << " " << ball.acceleration.y << std::endl;
             forces = Vector2Add(forces, {-100, 0});
         }
-        if(IsKeyDown(KEY_S)) {
-            //std::cout << ball.acceleration.x << " " << ball.acceleration.y << std::endl;
+        if (IsKeyDown(KEY_S))
+        {
+            // std::cout << ball.acceleration.x << " " << ball.acceleration.y << std::endl;
             forces = Vector2Add(forces, {0, 100});
         }
-        if(IsKeyDown(KEY_D)) {
-            //std::cout << ball.acceleration.x << " " << ball.acceleration.y << std::endl;
+        if (IsKeyDown(KEY_D))
+        {
+            // std::cout << ball.acceleration.x << " " << ball.acceleration.y << std::endl;
             forces = Vector2Add(forces, {100, 0});
+        }
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+
+            *dragLine0 = ballArray[0].position;
+            *dragLine1 = GetMousePosition();
+            // if (Vector2Distance(*dragLine0, *dragLine1) > 200)
+            // {
+            // }
+        }
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+        {
+            *dragLine0 = ballArray[0].position;
+            *dragLine1 = GetMousePosition();
+            // Multiplying forces by 10
+
+            std::cout << Vector2Distance(*dragLine0, *dragLine1) << std::endl;
+            forces = Vector2Add(forces, Vector2Scale(Vector2Subtract(*dragLine0, *dragLine1), 500.0f));
+            // delete[] dragLine0, dragLine1;
         }
 
         ballArray[0].acceleration = Vector2Scale(forces, ball.inverse_mass);
-        
+
         // Physics
         accumulator += delta_time;
-        while(accumulator >= TIMESTEP) {
-            for(int i = 0; i < numberOfCircles; i++){
+        while (accumulator >= TIMESTEP)
+        {
+            for (int i = 0; i < numberOfCircles; i++)
+            {
                 // std::cout << "i, k: " << i << " " << k << std::endl;
                 ballArray[i].velocity = Vector2Add(ballArray[i].velocity, Vector2Scale(ballArray[i].acceleration, TIMESTEP));
                 ballArray[i].velocity = Vector2Subtract(ballArray[i].velocity, Vector2Scale(ballArray[i].velocity, FRICTION * ballArray[i].inverse_mass * TIMESTEP));
                 ballArray[i].position = Vector2Add(ballArray[i].position, Vector2Scale(ballArray[i].velocity, TIMESTEP));
-                
-                if(ballArray[i].position.x + ballArray[i].radius >= WINDOW_WIDTH || ballArray[i].position.x - ballArray[i].radius <= 0) {
-                    ballArray[i].velocity.x *= -1;std::cout << ballArray[i].velocity.x << std::endl;
+
+                if (ballArray[i].position.x + ballArray[i].radius >= WINDOW_WIDTH || ballArray[i].position.x - ballArray[i].radius <= 0)
+                {
+                    ballArray[i].velocity.x *= -1;
+                    std::cout << ballArray[i].velocity.x << std::endl;
                 }
-                if(ballArray[i].position.y + ballArray[i].radius >= WINDOW_HEIGHT || ballArray[i].position.y - ballArray[i].radius <= 0) {   
-                    ballArray[i].velocity.y *= -1;std::cout << ballArray[i].velocity.y << std::endl;
+                if (ballArray[i].position.y + ballArray[i].radius >= WINDOW_HEIGHT || ballArray[i].position.y - ballArray[i].radius <= 0)
+                {
+                    ballArray[i].velocity.y *= -1;
+                    std::cout << ballArray[i].velocity.y << std::endl;
                 }
-                for(int k = 0; k < numberOfCircles; k++){
-                    if(k == i){
+                for (int k = 0; k < numberOfCircles; k++)
+                {
+                    if (k == i)
+                    {
                         k++;
                     }
-                    if(k >= numberOfCircles || i >= numberOfCircles){
+                    if (k >= numberOfCircles || i >= numberOfCircles)
+                    {
                         break;
                     }
-                    if(isCirclesColliding(ballArray[i], ballArray[k])){
+                    if (isCirclesColliding(ballArray[i], ballArray[k]))
+                    {
                         Vector2 n = Vector2Subtract(ballArray[i].position, ballArray[k].position); // currently swapped (as per sir's comment)
-                        float j = -( ((1+elasticityCoefficient)*Vector2DotProduct(Vector2Subtract(ballArray[i].velocity, ballArray[k].velocity/**/), n)) 
-                                    / (Vector2DotProduct(n,n)*(1/ballArray[i].mass)+(1/ballArray[k].mass)) );
-                        Vector2 newVelocity = Vector2Add(ballArray[i].velocity, Vector2Scale(n, (j/ballArray[i].mass) ));
+                        float j = -(((1 + elasticityCoefficient) * Vector2DotProduct(Vector2Subtract(ballArray[i].velocity, ballArray[k].velocity /**/), n)) / (Vector2DotProduct(n, n) * (1 / ballArray[i].mass) + (1 / ballArray[k].mass)));
+                        Vector2 newVelocity = Vector2Add(ballArray[i].velocity, Vector2Scale(n, (j / ballArray[i].mass)));
                         ballArray[i].velocity = newVelocity;
-                        Vector2 newVelocity2 = Vector2Subtract(ballArray[k].velocity, Vector2Scale(n, (j/ballArray[k].mass) ));
+                        Vector2 newVelocity2 = Vector2Subtract(ballArray[k].velocity, Vector2Scale(n, (j / ballArray[k].mass)));
                         ballArray[k].velocity = newVelocity2;
                     }
                 }
@@ -152,18 +217,19 @@ int main() {
 
         // pool table border
         ClearBackground(LIME);
-        DrawRectangle(75,0,650,40,RED); // Top Bar
-        DrawRectangle(0,75,40,450,RED); // Left Bar
-        DrawRectangle(760,75,40,450,RED); // Right Bar
-        DrawRectangle(75,560,650,40,RED); // Bottom Bar
-        
+        DrawRectangle(75, 0, 650, 40, RED);   // Top Bar
+        DrawRectangle(0, 75, 40, 450, RED);   // Left Bar
+        DrawRectangle(760, 75, 40, 450, RED); // Right Bar
+        DrawRectangle(75, 560, 650, 40, RED); // Bottom Bar
 
         // pool table corners
-        DrawCircleV(Vector2{37.5,37.5},37.5,BLACK); // Upper Left
-        DrawCircleV(Vector2{800-37.5,37.5},37.5,BLACK); // Upper Right
-        DrawCircleV(Vector2{37.5,600-37.5},37.5,BLACK); // Lower Left
-        DrawCircleV(Vector2{800-37.5,600-37.5},37.5,BLACK); //Lower Right
+        DrawCircleV(Vector2{37.5, 37.5}, 37.5, BLACK);             // Upper Left
+        DrawCircleV(Vector2{800 - 37.5, 37.5}, 37.5, BLACK);       // Upper Right
+        DrawCircleV(Vector2{37.5, 600 - 37.5}, 37.5, BLACK);       // Lower Left
+        DrawCircleV(Vector2{800 - 37.5, 600 - 37.5}, 37.5, BLACK); // Lower Right
 
+        // cue ball dragging
+        DrawLineV(*dragLine0, *dragLine1, YELLOW);
 
         EndDrawing();
     }
