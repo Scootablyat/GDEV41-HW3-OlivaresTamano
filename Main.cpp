e13590263fe43fca5436bbd62482ad860fc8ceba
JT <johnterra1215@gmail.com>
2024-09-28 22:43:33 +0800

    resolved merge conflicts

--- Main.cpp
+++ Main.cpp
@@ -23,10 +23,23 @@ struct Ball
     Vector2 velocity;
 };
 
+struct Border{
+    Vector2 position;
+    float width;
+    float height;
+    Color color;
+};
+
+struct Border{
+    Vector2 position;
+    float width;
+    float height;
+    Color color;
+};
+
 int toggleElasticity(int e)
 {
-    if (e == 0)
-    {
+    if (e == 0){
         return 1;
     }
     return 0;
@@ -38,8 +51,17 @@ float getDistance(Ball b1, Ball b2)
     return std::abs(Vector2Length(dist));
 }
 
-bool isCirclesColliding(Ball b1, Ball b2)
-{
+float getDistanceToPoint(Ball b1, Vector2 pos){
+    Vector2 dist = Vector2Subtract(b1.position, pos);
+    return std::abs(Vector2Length(dist));
+}
+
+float getDistanceToPoint(Ball b1, Vector2 pos){
+    Vector2 dist = Vector2Subtract(b1.position, pos);
+    return std::abs(Vector2Length(dist));
+}
+
+bool isCirclesColliding(Ball b1, Ball b2){
     float sumOfRadii = b1.radius + b2.radius;
     float distance = getDistance(b1, b2);
     if (distance <= sumOfRadii)
@@ -50,22 +72,70 @@ bool isCirclesColliding(Ball b1, Ball b2)
     return false;
 }
 
-// void DragCueBall(Vector2 *vector)
-// {
-// }
+bool isCircleCollidingWithCorner(Ball b1, Ball upperLeft, Ball upperRight, Ball lowerLeft, Ball lowerRight){
+    if(isCirclesColliding(b1,upperLeft)){
+        std::cout << "colliding with upper left" << std::endl;
+        return true;
+    }
+    if(isCirclesColliding(b1,upperRight)){
+        std::cout << "colliding with upper right" << std::endl;
+        return true;
+    }
+    if(isCirclesColliding(b1,lowerLeft)){
+        std::cout << "colliding with lower left" << std::endl;
+        return true;
+    }
+    if(isCirclesColliding(b1,lowerRight)){
+        std::cout << "colliding with lower right" << std::endl;
+        return true;
+    }
+    return false;
+}
 
-// float draglineX = abs(dragLine0->x - dragLine1->x);
-// float draglineY = abs(dragLine0->y - dragLine1->y);
-// if (draglineX > 200.0f)
-// {
-//     std::cout << Vector2Distance(*dragLine0, *dragLine1) << std::endl;
-//     dragLine1->x = dragLine0->x + 200.0f;
-// }
-// if (draglineY > 200.0f)
-// {
-//     // std::cout << Vector2Distance(*dragLine0, *dragLine1) << std::endl;
-//     dragLine1->y = dragLine0->y + 200.0f;
-// }
+bool isCircleCollidingWithBorder(Ball b1, Border br1){
+    float nearestX = fmaxf(br1.position.x, fminf(b1.position.x, br1.position.x + br1.width));
+    float nearestY = fmaxf(br1.position.y, fminf(b1.position.y, br1.position.y + br1.height));
+
+    float distance = getDistanceToPoint(b1, {nearestX, nearestY});
+    if (distance <= b1.radius){
+        return true;
+    }
+    return false;
+}
+
+
+
+bool isCircleCollidingWithCorner(Ball b1, Ball upperLeft, Ball upperRight, Ball lowerLeft, Ball lowerRight){
+    if(isCirclesColliding(b1,upperLeft)){
+        std::cout << "colliding with upper left" << std::endl;
+        return true;
+    }
+    if(isCirclesColliding(b1,upperRight)){
+        std::cout << "colliding with upper right" << std::endl;
+        return true;
+    }
+    if(isCirclesColliding(b1,lowerLeft)){
+        std::cout << "colliding with lower left" << std::endl;
+        return true;
+    }
+    if(isCirclesColliding(b1,lowerRight)){
+        std::cout << "colliding with lower right" << std::endl;
+        return true;
+    }
+    return false;
+}
+
+bool isCircleCollidingWithBorder(Ball b1, Border br1){
+    float nearestX = fmaxf(br1.position.x, fminf(b1.position.x, br1.position.x + br1.width));
+    float nearestY = fmaxf(br1.position.y, fminf(b1.position.y, br1.position.y + br1.height));
+
+    float distance = getDistanceToPoint(b1, {nearestX, nearestY});
+    if (distance <= b1.radius){
+        return true;
+    }
+    return false;
+}
+
 
 int main()
 {
@@ -97,6 +167,79 @@ int main()
     ball3.velocity = Vector2Zero();
 
     Ball ballArray[numberOfCircles] = {ball, ball2, ball3};
+
+
+    /*
+    DrawCircleV(Vector2{37.5,37.5},37.5,BLACK); // Upper Left
+    DrawCircleV(Vector2{800-37.5,37.5},37.5,BLACK); // Upper Right
+    DrawCircleV(Vector2{37.5,600-37.5},37.5,BLACK); // Lower Left
+    DrawCircleV(Vector2{800-37.5,600-37.5},37.5,BLACK); //Lower Right
+    */
+    Ball upperLeft;
+    upperLeft.position = {37.5,37.5};
+    upperLeft.radius = 37.5;
+    upperLeft.color = BLACK;
+    upperLeft.mass = 0.0f;
+    upperLeft.inverse_mass = 1 / ball.mass;
+    upperLeft.acceleration = Vector2Zero();
+    upperLeft.velocity = Vector2Zero();
+
+    Ball upperRight;
+    upperRight.position = {800-37.5,37.5};
+    upperRight.radius = 37.5;
+    upperRight.color = BLACK;
+    upperRight.mass = 0.0f;
+    upperRight.inverse_mass = 1 / ball.mass;
+    upperRight.acceleration = Vector2Zero();
+    upperRight.velocity = Vector2Zero();
+
+    Ball lowerLeft;
+    lowerLeft.position = {37.5,600-37.5};
+    lowerLeft.radius = 37.5;
+    lowerLeft.color = BLACK;
+    lowerLeft.mass = 0.0f;
+    lowerLeft.inverse_mass = 1 / ball.mass;
+    lowerLeft.acceleration = Vector2Zero();
+    lowerLeft.velocity = Vector2Zero();
+
+    Ball lowerRight;
+    lowerRight.position = {800-37.5,600-37.5};
+    lowerRight.radius = 37.5;
+    lowerRight.color = BLACK;
+    lowerRight.mass = 0.0f;
+    lowerRight.inverse_mass = 1 / ball.mass;
+    lowerRight.acceleration = Vector2Zero();
+    lowerRight.velocity = Vector2Zero();
+
+    /*
+    DrawRectangle(75,0,650,40,RED); // Top Bar
+    DrawRectangle(0,75,40,450,RED); // Left Bar
+    DrawRectangle(760,75,40,450,RED); // Right Bar
+    DrawRectangle(75,560,650,40,RED); // Bottom Bar
+    */
+    Border top;
+    top.position = {75,0};
+    top.width = 650;
+    top.height = 40;
+    top.color = RED;
+
+    Border left;
+    left.position = {0,75};
+    left.width = 40;
+    left.height = 450;
+    left.color = RED;
+
+    Border right;
+    right.position = {760,75};
+    right.width = 40;
+    right.height = 450;
+    right.color = RED;
+
+    Border bottom;
+    bottom.position = {75,560};
+    bottom.width = 650;
+    bottom.height = 40;
+    bottom.color = RED;
 
     int elasticityCoefficient = 0;
 
@@ -174,16 +317,24 @@ int main()
                 ballArray[i].velocity = Vector2Add(ballArray[i].velocity, Vector2Scale(ballArray[i].acceleration, TIMESTEP));
                 ballArray[i].velocity = Vector2Subtract(ballArray[i].velocity, Vector2Scale(ballArray[i].velocity, FRICTION * ballArray[i].inverse_mass * TIMESTEP));
                 ballArray[i].position = Vector2Add(ballArray[i].position, Vector2Scale(ballArray[i].velocity, TIMESTEP));
-
-                if (ballArray[i].position.x + ballArray[i].radius >= WINDOW_WIDTH || ballArray[i].position.x - ballArray[i].radius <= 0)
-                {
+                
+                if(ballArray[i].position.x + ballArray[i].radius >= WINDOW_WIDTH || ballArray[i].position.x - ballArray[i].radius <= 0) {
                     ballArray[i].velocity.x *= -1;
-                    std::cout << ballArray[i].velocity.x << std::endl;
                 }
-                if (ballArray[i].position.y + ballArray[i].radius >= WINDOW_HEIGHT || ballArray[i].position.y - ballArray[i].radius <= 0)
-                {
+                if(ballArray[i].position.y + ballArray[i].radius >= WINDOW_HEIGHT || ballArray[i].position.y - ballArray[i].radius <= 0) {   
                     ballArray[i].velocity.y *= -1;
-                    std::cout << ballArray[i].velocity.y << std::endl;
+                }
+                if(isCircleCollidingWithBorder(ballArray[i], top)){
+                    ballArray[i].velocity.y *= -1;
+                }
+                if(isCircleCollidingWithBorder(ballArray[i], bottom)){
+                    ballArray[i].velocity.y *= -1;
+                }
+                if(isCircleCollidingWithBorder(ballArray[i], left)){
+                    ballArray[i].velocity.x *= -1;
+                }
+                if(isCircleCollidingWithBorder(ballArray[i], right)){
+                    ballArray[i].velocity.x *= -1;
                 }
                 for (int k = 0; k < numberOfCircles; k++)
                 {
@@ -195,14 +346,17 @@ int main()
                     {
                         break;
                     }
-                    if (isCirclesColliding(ballArray[i], ballArray[k]))
-                    {
+                    
+                    if(isCirclesColliding(ballArray[i], ballArray[k])){
                         Vector2 n = Vector2Subtract(ballArray[i].position, ballArray[k].position); // currently swapped (as per sir's comment)
                         float j = -(((1 + elasticityCoefficient) * Vector2DotProduct(Vector2Subtract(ballArray[i].velocity, ballArray[k].velocity /**/), n)) / (Vector2DotProduct(n, n) * (1 / ballArray[i].mass) + (1 / ballArray[k].mass)));
                         Vector2 newVelocity = Vector2Add(ballArray[i].velocity, Vector2Scale(n, (j / ballArray[i].mass)));
                         ballArray[i].velocity = newVelocity;
                         Vector2 newVelocity2 = Vector2Subtract(ballArray[k].velocity, Vector2Scale(n, (j / ballArray[k].mass)));
                         ballArray[k].velocity = newVelocity2;
+                    }
+                    if(isCircleCollidingWithCorner(ballArray[i], upperLeft, upperRight, lowerLeft, lowerRight)){
+
                     }
                 }
             }
@@ -217,16 +371,17 @@ int main()
 
         // pool table border
         ClearBackground(LIME);
-        DrawRectangle(75, 0, 650, 40, RED);   // Top Bar
-        DrawRectangle(0, 75, 40, 450, RED);   // Left Bar
-        DrawRectangle(760, 75, 40, 450, RED); // Right Bar
-        DrawRectangle(75, 560, 650, 40, RED); // Bottom Bar
+        DrawRectangle(top.position.x, top.position.y, top.width, top.height,top.color); // Top Bar
+        DrawRectangle(left.position.x, left.position.y, left.width, left.height,left.color); // Left Bar
+        DrawRectangle(right.position.x, right.position.y, right.width, right.height,right.color); // Right Bar
+        DrawRectangle(bottom.position.x, bottom.position.y ,bottom.width, bottom.height,bottom.color); // Bottom Bar
+        
 
         // pool table corners
-        DrawCircleV(Vector2{37.5, 37.5}, 37.5, BLACK);             // Upper Left
-        DrawCircleV(Vector2{800 - 37.5, 37.5}, 37.5, BLACK);       // Upper Right
-        DrawCircleV(Vector2{37.5, 600 - 37.5}, 37.5, BLACK);       // Lower Left
-        DrawCircleV(Vector2{800 - 37.5, 600 - 37.5}, 37.5, BLACK); // Lower Right
+        DrawCircleV(upperLeft.position, upperLeft.radius, upperLeft.color); // Upper Left
+        DrawCircleV(upperRight.position, upperRight.radius, upperRight.color); // Upper Right
+        DrawCircleV(lowerLeft.position, lowerLeft.radius, lowerLeft.color); // Lower Left
+        DrawCircleV(lowerRight.position, lowerRight.radius, lowerRight.color); //Lower Right
 
         // cue ball dragging
         DrawLineV(*dragLine0, *dragLine1, YELLOW);
